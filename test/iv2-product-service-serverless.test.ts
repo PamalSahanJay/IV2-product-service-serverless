fdcaf91125e8c7968d7ee3905a6b8e100ea47985
psahan <psahan@wiley.com>
2024-09-20 18:19:07 +0530

    init app

---
+++ test/iv2-product-service-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Iv2ProductServiceServerless from '../lib/iv2-product-service-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iv2-product-service-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Iv2ProductServiceServerless.Iv2ProductServiceServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
